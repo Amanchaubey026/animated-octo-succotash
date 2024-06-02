6c83692848df25a1c094f76bde464647efcec30c
Aman Chaubey <amanchaubey86@gmail.com>
2024-06-02 12:16:14 +0530

    wegmmi configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-// import React from 'react'
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+import { Event } from "./components/Event";
+import { FAQ } from "./components/FAQ";
+import { Footer } from "./components/Footer";
+import { HeroBanner } from "./components/HeroBanner";
+import { Introduction } from "./components/Introduction";
+import { RoadMap } from "./components/RoadMap";
+import { Tokenomics } from "./components/Tokenomics";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Box>
+      <HeroBanner />
+      <Introduction />
+      <Event />
+      <Tokenomics />
+      <RoadMap />
+      <FAQ />
+      <Footer />
+    </Box>
+  );
 }
 
-export default App
+export default App;
